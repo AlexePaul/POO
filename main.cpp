21aaa5ca2286e886a8b540b696601fa55a36a8fd
AlexePaul <alexepaul2011@yahoo.com>
2022-03-16 09:37:00 +0200

    Finished Test Functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
+#include "student.h"
+#include "grupa.h"
 #include <iostream>
 
 using namespace std;
 
+void fluxTestare1(){
+	grupa gr1, gr2;
+	cin >> gr1 >> gr2;
+	student st, st2;
+	cin >> st >> st2;
+	gr1.adaugaStudent(st);
+	gr1.scoateStudent(st2.getNume());
+	cout << "grupa1: \n" << gr1 << '\n';
+	cout << "grupa2: \n" << gr2 << '\n';
+}
+
+void fluxTestare2(){
+	grupa gr1;
+	cin >> gr1;
+	student st;
+	cin >> st;
+	cout << "Media Grupei inainte de adaugarea noului student: " << gr1.mediaGrupei() << '\n';
+	gr1.adaugaStudent(st);
+	cout << "Media Grupei dupa de adaugarea noului student: " << gr1.mediaGrupei() << '\n'	;
+}
+
 int main(){
-	cout << "Hello World!";
+	fluxTestare1();
+	fluxTestare2();
 	return 0;
 }
